58a9f405d5a890f6a3073633668e49047d6db66b
Bas Boot <github@basboot.nl>
2025-05-07 20:07:56 +0200

    Change resolution to 16:10

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 import "../css/style.css";
-import {Engine, DisplayMode, SolverStrategy, Timer} from "excalibur";
+import { Engine, DisplayMode, SolverStrategy, Timer } from "excalibur";
 import { Resources, ResourceLoader } from "./resources.js";
 import { Level } from "./level.js";
 import { MusicPlayer } from "./musicPlayer.js";
@@ -8,7 +8,7 @@ export class Game extends Engine {
   constructor() {
     super({
       width: 800,
-      height: 450,
+      height: 500,
       maxFps: 60,
       displayMode: DisplayMode.FitScreen,
       physics: {
@@ -24,13 +24,13 @@ export class Game extends Engine {
 
   async startGame() {
     // Load the MIDI data
-    const response = await fetch("/public/music/autobahn2.json");
+    const response = await fetch("/public/music/popcorn1.json");
     const midiData = await response.json();
 
     // Initialize and play the music
     const musicPlayer = new MusicPlayer(midiData);
     musicPlayer.setVolume(this.soundLevel); // Set the initial sound level
-    if (true) {
+    if (false) {
       // TODO: create better sounds
       musicPlayer.play();
     }
@@ -38,7 +38,6 @@ export class Game extends Engine {
     const level = new Level(); // Create the Level scene
     this.add("level", level); // Add the scene to the game
     await this.goToScene("level"); // Switch to the Level scene
-
   }
 }
 
