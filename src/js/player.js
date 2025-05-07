e8f943a4ed451d9e1265442ea19812766f2ffbac
Bas Boot <github@basboot.nl>
2025-05-07 21:04:09 +0200

    Add jump

--- src/js/player.js
+++ src/js/player.js
@@ -1,17 +1,29 @@
-import {Actor, Keys, Vector} from "excalibur";
+import { Actor, CollisionType, Keys, Vector } from "excalibur";
 import { CarSprites } from "./resources";
-import {Car} from "./car.js";
+import { Car } from "./car.js";
 
 export class Player extends Car {
+  jumping;
+  jumpTime;
+
   constructor(carType, initialPosition, initialVelocity, minSpeed, maxSpeed) {
     super(carType, initialPosition, initialVelocity, minSpeed, maxSpeed);
   }
 
   onInitialize(engine) {
+    this.jumping = false;
     this.body.mass = 1000;
-    super.onInitialize(engine);
 
-   }
+    engine.input.keyboard.on("press", (evt) => {
+      if (evt.key === Keys.Space) {
+        this.body.collisionType = CollisionType.PreventCollision;
+        this.jumpTime = this.scene.engine.clock.now();
+        this.jumping = true;
+      }
+    });
+
+    super.onInitialize(engine);
+  }
 
   update(engine, delta) {
     // console.log(this.pos);
@@ -26,6 +38,16 @@ export class Player extends Car {
     }
     if (engine.input.keyboard.isHeld(Keys.ArrowRight)) {
       this.steerRight();
+    }
+
+    if (this.jumping) {
+      const t = (engine.clock.now() - this.jumpTime) * 0.0005 * Math.PI;
+      if (t >= Math.PI) {
+        this.body.collisionType = CollisionType.Active;
+        this.jumping = false;
+      }
+      const s = 1 + Math.sin(t);
+      this.scale = new Vector(s, s);
     }
 
     super.update(engine, delta);
